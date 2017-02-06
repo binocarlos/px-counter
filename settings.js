49e5e2f8c3eb455f646242ae60407f72ebfc9ddf
Kai Davenport <kaiyadavenport@gmail.com>
2017-02-06 23:17:54 +0000

    use different env name

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ var args = require('minimist')(process.argv, {
     t: 'test'
   },
   default:{
-    port: process.env.PORT || 8080,
+    port: process.env.LISTEN_PORT || 8080,
     filepath: process.env.FILEPATH || '/tmp/px-counter-items.json'
   },
   boolean:['test']
