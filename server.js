eb78560543d6c05fda15b0bb53871d4de601bda8
Kai Davenport <kaiyadavenport@gmail.com>
2017-02-06 23:20:58 +0000

    add logging

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ module.exports = function(opts){
     console.log('creating filepath: ' + filePath)
     fs.writeFileSync(filePath, JSON.stringify([]))
   }
+  else {
+    console.log('exiting data found')
+    console.log(fs.readFileSync(filePath))
+  }
 
   router.addRoute("/v1/items", {
     GET: function (req, res) {
@@ -32,6 +36,8 @@ module.exports = function(opts){
     POST: function (req, res) {
       req.pipe(concat(function(data){
         var jsonData = data.toString()
+        console.log('writing data')
+        console.log(jsonData)
         fs.writeFile(filePath, jsonData, 'utf8', function(err) {
           if(err) {
             res.statusCode = 500
